c284634abdeebc567c39a6367515d8decbf13af3
Andre Targino <andretargino@3071-ad-05075.wucon.wustl.edu>
2023-11-13 11:59:18 -0600

    Add dilation

--- Morph/operators.py
+++ Morph/operators.py
@@ -3,3 +3,7 @@ import skimage
 
 def erosion(image, element=None):
     return skimage.morphology.erosion(image, element)
+
+
+def dilation(image, element=None):
+    return skimage.morphology.dilation(image, element)
