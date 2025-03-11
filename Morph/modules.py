da25cd4bdaaa91f44f1ce04c71e81369798c8f92
Andre Targino <andretargino@3071-AD-05075.local>
2025-03-11 17:26:25 -0500

    Add Xenium mapper

--- Morph/modules.py
+++ Morph/modules.py
@@ -1,3 +1,10 @@
+import numpy
+
+
+def _floor(x):
+    return numpy.astype(x, int)
+
+
 class Mapper:
     def naive(self, data):
         return data
@@ -5,3 +12,8 @@ class Mapper:
     def visium(self, data):
         x = (data['x'] + data['y']) // 2
         return {'g': data['g'], 'x': x, 'y': data['y']}
+
+    def xenium(self, data, d):
+        x = _floor(data['x'] / d)
+        y = _floor(data['y'] / d)
+        return {'g': data['g'], 'x': x, 'y': y}
