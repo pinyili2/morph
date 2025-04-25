2cfccb342a9eef1ded407f08401c394e63e8b577
Andre Targino <andretargino@3071-AD-05075.local>
2025-04-24 19:40:51 -0500

    Add ultimate center

--- Morph/features.py
+++ Morph/features.py
@@ -17,6 +17,10 @@ def _nonzero(a):
     return numpy.nonzero(a)
 
 
+def _any(a):
+    return numpy.any(a)
+
+
 class Center():
     def geodesic(self, image, element=None):
         propagation = Morph.operators.propagation_function(image, element)
@@ -26,4 +30,18 @@ class Center():
         for i, m in zip(index, minimum):
             points = _nonzero((image == i) & (propagation == m))
             centers[i] = set(zip(*points))
+        return centers
+
+    def ultimate(self, image, element=None):
+        index = _unique(image)
+        centers = {i: set() for i in index}
+        while _any(image):
+            eroded = Morph.operators.erosion(image, element)
+            reconstructed = Morph.operators.reconstruction_by_dilation(
+                eroded, image, element)
+            points = _nonzero(image - reconstructed)
+            for point in zip(*points):
+                i = image[point]
+                centers[i].add(point)
+            image = eroded
         return centers
